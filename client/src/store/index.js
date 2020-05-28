400a2739ed5575393b21a0d92461972aab7deb14
Marko <markogajiin@gmail.com>
2020-05-28 20:16:29 +0200

    client presets

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import styles from '../assets/theme/main.scss';
+
+Vue.use(styles)
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  modules: {}
 })
