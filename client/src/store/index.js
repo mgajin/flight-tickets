f5dc8b4787c35f7ef622f2f8b13b424f5b6176e8
Marko <markogajiin@gmail.com>
2020-06-02 01:57:53 +0200

    fix

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import flights from './modules/flights';
+import flights from './modules/flights'
+import auth from './modules/auth'
 
-import styles from '../assets/theme/main.scss';
+import styles from '../assets/theme/main.scss'
 
 Vue.use(styles)
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    flights
+    flights,
+    auth
   }
 })
