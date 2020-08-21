a197c1c1df4c9559ea4b2512106e4d7f9aeb5a04
Marko <markogajiin@gmail.com>
2020-08-22 00:33:21 +0200

    flights module implemented

--- client/src/store/modules/flights.js
+++ client/src/store/modules/flights.js
@@ -1,10 +1,32 @@
-const state = {}
+import Axios from 'axios'
+const URL = 'http://localhost:3000/app/flights'
 
-const getters = {}
+const state = {
+    flights: []
+}
 
-const mutations = {}
+const getters = {
+    getFlights: state => state.flights
+}
 
-const actions = {}
+const mutations = {
+    set_flights: (state, flights) => state.flights = flights
+}
+
+const actions = {
+
+    async GET_FLIGHTS({ commit }) {
+        Axios.get(URL)
+            .then(response => {
+                const { flights } = response.data
+                commit('set_flights', flights)
+            })
+            .catch(err => {
+                const { message } = err.response.data
+                alert(message)
+            })
+    }
+}
 
 export default {
     state,
